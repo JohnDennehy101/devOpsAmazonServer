b7e6d782e4e2857cc55ad43df63f1124c3dfffc9
JohnDennehy <johndennehy101@gmail.com>
2021-02-28 14:54:39 +0000

    Added functionality for image upload to s3. Public permissions now set and content type set to image/jpeg to ensure that object url is displayed in browser.

--- newwebserver.py
+++ newwebserver.py
@@ -62,4 +62,24 @@ def create_bucket(bucket_name, region=None):
         return False
     return True
 
+
+def upload_file(file_name, bucket, object_name=None):
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name, ExtraArgs={
+        'ACL':'public-read',
+        'ContentType': "image/jpeg",
+    })
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
 create_bucket(s3BucketName, 'eu-west-1')
+upload_file('./imageToBeUploaded.jpg', s3BucketName, 'webSiteImage.jpg')
