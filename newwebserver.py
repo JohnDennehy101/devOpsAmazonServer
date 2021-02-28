7270bd49feb2cdcf1a7d82feffcb2cdc08de0b61
JohnDennehy <johndennehy101@gmail.com>
2021-02-28 17:46:15 +0000

    Added wait_until_running waiter and reload methods to enable programme to access public ip address of ec2 instance.

--- newwebserver.py
+++ newwebserver.py
@@ -5,6 +5,7 @@ from botocore.exceptions import ClientError
 import logging
 import requests
 from time import gmtime
+import subprocess
 
 serverCommands = """#!/bin/bash 
 yum update -y 
@@ -12,6 +13,7 @@ yum install httpd -y
 systemctl enable httpd 
 systemctl start httpd"""
 ec2 = boto3.resource('ec2')
+print("Starting instance")
 new_instance = ec2.create_instances(
                                     ImageId='ami-0fc970315c2d38f01',
                                     MinCount=1,
@@ -31,7 +33,12 @@ new_instance = ec2.create_instances(
             ]
         },
     ],)
+
+new_instance[0].wait_until_running()
 print (new_instance[0].id)
+new_instance[0].reload()
+print(new_instance[0].public_ip_address)
+instanceIpAddress = new_instance[0].public_ip_address
 
 imageUrl = 'http://devops.witdemo.net/image.jpg'
 imageContent = requests.get(imageUrl).content
@@ -81,5 +88,8 @@ def upload_file(file_name, bucket, object_name=None):
         return False
     return True
 
-create_bucket(s3BucketName, 'eu-west-1')
-upload_file('./imageToBeUploaded.jpg', s3BucketName, 'webSiteImage.jpg')
+#create_bucket(s3BucketName, 'eu-west-1')
+#upload_file('./imageToBeUploaded.jpg', s3BucketName, 'webSiteImage.jpg')
+
+sshCommand = "ssh -o StrictHostKeyChecking=no -i credentials.pem ec2-user" + instanceIpAddress + " 'pwd' "
+print(sshCommand)
