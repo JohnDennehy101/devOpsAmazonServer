3b0f1ece9fc142081344110668e6fcc439e1b328
JohnDennehy <johndennehy101@gmail.com>
2021-02-28 14:32:16 +0000

    Added boto configuration for creating s3 bucket.

--- newwebserver.py
+++ newwebserver.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 import sys
 import boto3
+from botocore.exceptions import ClientError
+import logging
 import requests
+from time import gmtime
 
 serverCommands = """#!/bin/bash 
 yum update -y 
@@ -35,3 +38,28 @@ imageContent = requests.get(imageUrl).content
 
 with open('./imageToBeUploaded.jpg', 'wb') as handleImageData:
     handleImageData.write(imageContent)
+
+
+currentTime = gmtime()
+s3BucketName = 'imagebucket.' + str(currentTime.tm_mday) + '-' + str(currentTime.tm_mon) + '-' + str(currentTime.tm_year) + '.' + str(currentTime.tm_hour) + '.' + str(currentTime.tm_min) + '.' + str(currentTime.tm_sec)
+
+
+
+def create_bucket(bucket_name, region=None):
+
+    # Create bucket
+    try:
+        if region is None:
+            s3_client = boto3.client('s3')
+            s3_client.create_bucket(Bucket=bucket_name)
+        else:
+            s3_client = boto3.client('s3', region_name=region)
+            location = {'LocationConstraint': region}
+            s3_client.create_bucket(Bucket=bucket_name,
+                                    CreateBucketConfiguration=location)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+create_bucket(s3BucketName, 'eu-west-1')
